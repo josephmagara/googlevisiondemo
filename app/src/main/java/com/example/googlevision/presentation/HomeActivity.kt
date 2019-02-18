bc8f780594e31064b268b902749aa9042e43be7f
Joseph Magara <joseph.magara16@gmail.com>
2019-02-19 10:38:49 +1100

    Initialized project

---
+++ app/src/main/java/com/example/googlevision/presentation/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.googlevision.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.googlevision.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
