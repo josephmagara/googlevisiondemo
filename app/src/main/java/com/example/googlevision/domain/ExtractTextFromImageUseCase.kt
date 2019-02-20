0b2ab1b44d687c95bc96c42dc65cd29aa5a2cd60
Joseph Magara <joseph.magara16@gmail.com>
2019-02-20 12:40:07 +1100

    Started the process of extracting the text from the image

---
+++ app/src/main/java/com/example/googlevision/domain/ExtractTextFromImageUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.googlevision.domain
+
+import android.graphics.Bitmap
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import javax.inject.Inject
+
+/**
+ * Created by josephmagara on 20/2/19.
+ */
+class ExtractTextFromImageUseCase @Inject constructor(){
+
+}
