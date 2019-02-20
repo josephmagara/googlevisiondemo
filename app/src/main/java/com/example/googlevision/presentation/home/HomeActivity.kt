17f1a0e02021ae74c3e320e0775be3333c5d4461
Joseph Magara <joseph.magara16@gmail.com>
2019-02-20 12:42:43 +1100

    Code clean up

--- app/src/main/java/com/example/googlevision/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/example/googlevision/presentation/home/HomeActivity.kt
@@ -2,7 +2,6 @@ package com.example.googlevision.presentation.home
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
--- app/src/main/java/com/example/googlevision/util/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/example/googlevision/util/extensions/ActivityExtensions.kt
@@ -1,16 +1,29 @@
 package com.example.googlevision.util.extensions
 
+import android.Manifest
 import android.app.Activity
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Context.CAMERA_SERVICE
+import android.content.pm.PackageManager
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
 import android.os.Build
+import android.os.Environment
 import android.util.Log
 import android.view.Surface
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.googlevision.util.CAMERA_PERMISSION_REQUEST_CODE
+import com.example.googlevision.util.DATE_FORMAT
+import com.example.googlevision.util.READ_STORAGE_PERMISSION_REQUEST_CODE
+import com.example.googlevision.util.WRITE_TO_STORAGE_PERMISSION_REQUEST_CODE
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 /**
@@ -21,15 +34,50 @@ private val ORIENTATIONS = intArrayOf(
     Surface.ROTATION_0, 90,
     Surface.ROTATION_90, 0,
     Surface.ROTATION_180, 270,
-    Surface.ROTATION_270, 180)
+    Surface.ROTATION_270, 180
+)
 
 /**
  * Get the angle by which an image must be rotated given the device's current
  * orientation.
  */
+
+
+val permissionList = mapOf(
+    Manifest.permission.CAMERA to CAMERA_PERMISSION_REQUEST_CODE,
+    Manifest.permission.READ_EXTERNAL_STORAGE to READ_STORAGE_PERMISSION_REQUEST_CODE,
+    Manifest.permission.WRITE_EXTERNAL_STORAGE to WRITE_TO_STORAGE_PERMISSION_REQUEST_CODE
+)
+
+fun Activity.hasAllNeededPermissions(permissions: Map<String, Int> = permissionList): Boolean =
+    permissions.keys.all { !permissionDenied(this, it) }
+
+fun Activity.requestPermissions(
+    permissions: Map<String, Int> = permissionList
+): Boolean {
+
+    var requestCode = 1
+
+    val nonGrantedPermissionValues = permissions
+        .filter { permission ->
+            permissionDenied(this, permission.key)
+                .also { isNotGranted ->
+                    if (isNotGranted) {
+                        requestCode *= permission.value
+                    }
+                }
+        }.map { it.key }.toTypedArray()
+
+    if (nonGrantedPermissionValues.isNotEmpty()) {
+        ActivityCompat.requestPermissions(this, nonGrantedPermissionValues, requestCode)
+    }
+
+    return nonGrantedPermissionValues.isNotEmpty()
+}
+
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 @Throws(CameraAccessException::class)
-private fun Activity.getRotationCompensation(cameraId: String): Int {
+fun Activity.getRotationCompensation(cameraId: String): Int {
     // Get the device's current rotation relative to its "native" orientation.
     // Then, from the ORIENTATIONS table, look up the angle the image must be
     // rotated to compensate for the device's rotation.
@@ -59,3 +107,17 @@ private fun Activity.getRotationCompensation(cameraId: String): Int {
     }
     return result
 }
+
+fun Activity.createFile(fileName: String): File {
+    val timeStamp: String = SimpleDateFormat(DATE_FORMAT).format(Date())
+    val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return File.createTempFile(
+        "JPEG_${fileName}_$timeStamp", /* prefix */
+        ".jpg", /* suffix */
+        storageDir /* directory */
+    )
+}
+
+
+private fun permissionDenied(context: Context, permission: String) =
+    ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_DENIED
